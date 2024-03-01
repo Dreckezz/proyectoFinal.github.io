d35993c52c8cb8b57fae089838812876b62e38a1
Dreckezz <148512368+Dreckezz@users.noreply.github.com>
2024-03-01 11:12:03 -0300

    actualizacion de codigo

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+var settings = {
+  timeout: 2000, // Optional.
+  logError: true // Optional.
+}
+var p = new Ping(settings);
+
 // ------------------------------------------------------------------------------------------------------
 
 // SALUDO DE BIENVENIDA
@@ -47,38 +54,55 @@ btnCerrarModal.addEventListener("click",(event)=>{
 })
 
 // ------------------------------------------------------------------------------------------------------
-
+var notificacionesPendientes = [];
 var listaDatos = [];
-
-// ...
 
 // Función para agregar una fila a la tabla
 function agregarFila(identificador, direccion, estado) {
   var containerDiv = document.getElementById('tabla-container');
   var newRow = containerDiv.insertRow(-1);
-  newRow.style.border = "1px solid";
 
   // Añadir celdas para nombre, URL, estado y el botón "X"
   var identificadorCell = newRow.insertCell(0);
   var direccionCell = newRow.insertCell(1);
   var estadoCell = newRow.insertCell(2);
   var accionesCell = newRow.insertCell(3);
+  p.ping(direccion, function(err, data) {
+    // Also display error if err is returned.
+    identificadorCell.innerHTML = identificador;
+    direccionCell.innerHTML = direccion;
+    if (err) {
+      console.log("error loading resource")
+      data = data + " " + err;
+      estadoCell.innerHTML = "Inactivo";
+      emailjs.send("service_r6wv88b","template_d3fvfgv",{
+        nombre: "Patricio",
+        email: "patricioajv@gmail.com",
+        mensaje: "No esta activo",
+        sitio: direccion,
+      });
+    }
+    else{
+      var estadoFinal = "Activo ping de:  "+data;
+      estadoCell.innerHTML = estadoFinal;
+    }
+    
+    
 
-  identificadorCell.innerHTML = identificador;
-  direccionCell.innerHTML = direccion;
-  estadoCell.innerHTML = estado;
+    // Crear el botón "X" y asignarle un evento de clic
+    var botonX = document.createElement('button');
+    botonX.innerHTML = 'X';
+    botonX.addEventListener('click', function () {
+        // Eliminar la fila al hacer clic en el botón "X"
+        containerDiv.deleteRow(newRow.rowIndex);
+        listaDatos.splice(newRow.rowIndex - 1, 1); // Eliminar el elemento correspondiente en la listaDatos
+        guardarDatosLocalStorage(); // Actualizar el almacenamiento local
+    });
 
-  // Crear el botón "X" y asignarle un evento de clic
-  var botonX = document.createElement('button');
-  botonX.innerHTML = 'X';
-  botonX.addEventListener('click', function () {
-      // Eliminar la fila al hacer clic en el botón "X"
-      containerDiv.deleteRow(newRow.rowIndex);
-      listaDatos.splice(newRow.rowIndex - 1, 1); // Eliminar el elemento correspondiente en la listaDatos
-      guardarDatosLocalStorage(); // Actualizar el almacenamiento local
+    accionesCell.appendChild(botonX);
   });
 
-  accionesCell.appendChild(botonX);
+  
 }
 
 // Función para guardar datos en el almacenamiento local
@@ -97,40 +121,111 @@ function recuperarDatosLocalStorage() {
       }
   }
 }
+function enviarNotificacionesPendientes() {
+  if (notificacionesPendientes.length > 0) {
+    // Enviar un solo correo electrónico con todas las notificaciones
+    emailjs.send("service_r6wv88b", "template_d3fvfgv", {
+      notificaciones: JSON.stringify(notificacionesPendientes),
+    });
+
+    // Limpiar la lista de notificaciones pendientes
+    notificacionesPendientes = [];
+  }
+}
+var intervalId;
+// funcion para verificar estados de las url
+function verificarEstadoURLs(tiempo) {
+  intervalId = setInterval(function () {
+    for (var i = 0; i < listaDatos.length; i++) {
+      (function (index) {
+        p.ping(listaDatos[index].direccion, function (err, data) {
+          // Actualizar el estado en la lista de datos
+          if (err) {
+            listaDatos[index].estado = "Inactivo";
+            if (!listaDatos[index].notificacionEnviada) {
+              notificacionesPendientes.push({
+                nombre: "Patricio",
+                email: "patricioajv@gmail.com",
+                mensaje: "ha dejado de estar activo",
+                sitio: listaDatos[index].direccion,
+              });
+
+              // Marcar la notificación como enviada
+              listaDatos[index].notificacionEnviada = true;
+            }
+
+          } 
+          else {
+            listaDatos[index].notificacionEnviada = false;
+            listaDatos[index].estado = "Activo ping de: " + data;
+          }
+
+          // Actualizar la celda correspondiente en la tabla
+          var estadoCell = document.getElementById("tabla-container").rows[index + 1].cells[2];
+          estadoCell.innerHTML = listaDatos[index].estado;
+        });
+      })(i);
+    }
+    enviarNotificacionesPendientes();
+  }, tiempo);
+}
 
 
 
-
-
-// ...
-
+// ------------------------------------------------------------------------------------------------------
 // CAPTURA DE DATOS MEDIANTE FORMULARIO
 var formulario = document.querySelector("form");
 formulario.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  identificador = document.querySelector("#nombre").value;
-  direccion = document.querySelector("#url").value;
+  var identificador = document.querySelector("#nombre").value;
+  var direccion = document.querySelector("#url").value;
 
   if (identificador != '' && direccion != '') {
-      var dato = {
-          identificador,
-          direccion,
-          estado: 'inactivo'
-      };
+    var dato = {
+      identificador,
+      direccion,
+      estado: 'inactivo',
+      notificacionEnviada: false,
+    };
 
-      listaDatos.push(dato);
+    listaDatos.push(dato);
 
-      // Agregar la nueva fila con el botón "X"
-      agregarFila(identificador, direccion, dato.estado);
-      document.querySelector("#nombre").value = "";
-      document.querySelector("#url").value = "";
+    // Agregar la nueva fila con el botón "X"
+    agregarFila(identificador, direccion, dato.estado);
+    document.querySelector("#nombre").value = "";
+    document.querySelector("#url").value = "";
 
-      guardarDatosLocalStorage(); // Actualizar el almacenamiento local
-  } else {
+    guardarDatosLocalStorage(); // Actualizar el almacenamiento local
+  } 
+  else {
       alert("No dejes campos vacíos.");
   }
 });
 
 // Recuperar datos almacenados al cargar la página
 recuperarDatosLocalStorage();
+
+//-----------------------------------------------------------------------------------------
+var elementoSeleccionado;
+if (localStorage.getItem('elementoSeleccionado')) {
+  elementoSeleccionado = localStorage.getItem('elementoSeleccionado');
+} else {
+  elementoSeleccionado = document.getElementById("timeSelect").value;
+}
+
+// Asignar el valor inicial al elemento seleccionado
+document.getElementById("timeSelect").value = elementoSeleccionado;
+
+// Event listener para el cambio en el selector de tiempo
+var elementoSeleccionadoInput = document.getElementById("timeSelect");
+elementoSeleccionadoInput.addEventListener("change", function () {
+  elementoSeleccionado = elementoSeleccionadoInput.value;
+  localStorage.setItem('elementoSeleccionado', elementoSeleccionado);
+  clearInterval(intervalId);
+  if (elementoSeleccionado !== "Verificar estado y ping") {
+    verificarEstadoURLs(elementoSeleccionado);
+  } 
+  else {
+  }
+});
